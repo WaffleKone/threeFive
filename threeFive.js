f81ad58869893ed3662fff5a3b2305caa5879658
Waffle <88282807+WaffleKone@users.noreply.github.com>
2022-06-30 19:38:48 -0400

    Refactored using % operator.

--- threeFive.js
+++ threeFive.js
@@ -1,17 +1,21 @@
 function threeFive(startVal, endVal) {
     let currentVal = startVal;
+    console.time("tresCinco")
     for(let i=startVal; i<=endVal; i++) {
-        if(Number.isInteger(currentVal/3)&(Number.isInteger(currentVal/5))){
-            console.log('threeFive')
-        } else if (Number.isInteger(currentVal/3)) {
-            console.log('three')
-        } else if (Number.isInteger(currentVal/5)) {
-            console.log('five')
-        } else {
+        if (currentVal % 3 && currentVal % 5) {
             console.log(currentVal)
+        } else if (currentVal % 3) {
+            console.log('five')
+        } else if (currentVal % 5) {
+            console.log('three')
+        } else {
+            console.log('threefive')
         }
         currentVal++
     }
+    console.timeEnd("tresCinco")
 }
 
 threeFive(1, 100)
+
+
